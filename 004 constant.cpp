22d845cf5cff932239ce31a80bc258e342b8c323
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-24 23:14:42 +0600

    Create 004 constant.cpp
    Constant

---
+++ 004 constant.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// Define constant using #define
+#define A 20
+#define B 15
+#define N endl
+
+int main()
+{
+    // Define constant using const Keyword
+    const int  L = 10;
+    const int  W  = 5;
+    const char NEWLINE = '\n';
+
+    cout << "Summation of A and B is: " << A + B << N;
+
+    cout << "L is : " << L << NEWLINE;
+
+    return 0;
+}
