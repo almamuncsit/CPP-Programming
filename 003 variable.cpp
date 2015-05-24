363215a07ad1a91cf3f2324f1ece1a4dbb70c1e0
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-24 23:04:31 +0600

    Create 003 variable.cpp
    Declare different types variable

---
+++ 003 variable.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char c = 'a'; //declare char type variable
+    int a = 15, b = 25; //declare Integer type variable
+    float f = 20.24;  //declare Float type variable
+    double d = 154573.254;  //declare Double type variable
+
+
+    // Print all variable
+    cout << "Char is: " << c << endl; // endl is use for new line
+    cout << "Integer a is: " << c << "b is: " << b << endl;
+    cout << "Float F is: " << f << endl;
+    cout << "Double d is: "<< d << endl;
+
+
+    return 0;
+}
