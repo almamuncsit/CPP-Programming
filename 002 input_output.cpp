1ce1d2949197a94cde3c1d833367acc7a7bac258
Md. Al-Mamun Sarkar <almamun.csit@gmail.com>
2015-05-24 22:50:01 +0600

    Create 002 input_output.cpp
    Input output

---
+++ 002 input_output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+// main() is where program execution begins.
+int main()
+{
+    int a;
+    // Show message before getting input
+    cout<<"Please enter a number: ";
+    // Get input from user
+    cin>>a;
+
+    // Show output
+    cout<<"Number is: "<<a<<endl;
+
+    return 0;
+}
